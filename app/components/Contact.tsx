f0677440f47b5d7b3a3a3630ea685238c61ef66b
Alejo Bentos <bentosalejo@gmail.com>
2023-08-15 19:51:20 -0300

    commit

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -4,7 +4,12 @@ import { Box, Typography } from "@mui/material"
 
 const Contact = () => {
     return(
-        <>
+        
+        <Box sx={{width:'100%'}}>
+
+
+
+
             <Box sx={{
                 backgroundImage:'url("/images/anuel.png")',
                 width:'100%',
@@ -16,16 +21,36 @@ const Contact = () => {
                 backgroundSize:'cover',
                 }}>
             <Box sx={{
-                backgroundColor:'#fb8fe5',
+                // backgroundColor:'#fb8fe5',
+                backgroundColor: 'rgba(200, 200, 200, 0.8)',
                 width:'100%',
                 height:'200px',
-                opacity:'0.5'
-                
+                display:'flex',
+                alignItems:'center',
+                justifyContent:'center'
                 }}>
+                <Typography sx={{fontWeight:'bold', color:'#000', fontFamily:'fantasy', letterSpacing:'.5em', fontSize:'2rem'}}>CONTACTO</Typography>
             </Box>
             </Box>
-            <Typography sx={{fontWeight:'bold', color:'#000', position:'absolute', top:250, fontFamily:'fantasy', letterSpacing:'.5em', fontSize:'2rem'}}>CONTACTO</Typography>
-        </>
+            <Box sx={{backgroundColor:'#fff', display:'flex', flexDirection:'column'}}>
+                <Box component={'img'} alt="looogggoo" src="/images/diamond.jpg"/>
+                <Box sx={{display:'flex', flexDirection:'column', textAlign:'center', padding:'0 30px 30px 30px'}}>
+                    <Box component={'h2'}>CONSULTA POST COMPRA</Box>
+                    <Typography sx={{margin:'15px 0 15px 0', lineHeight:'1.8em', letterSpacing:'0.2em'}}>
+                    
+                    Para realizar una consulta sobre una compra realizada en nuestro sitio web, le solicitamos que indique la venta Jewerly Proyect ID de venta -  estos se pueden encontrar en el correo electrónico de confirmación de compra que ha recibido.
+                    </Typography>
+                </Box>
+            </Box>
+
+
+
+
+
+
+
+        </Box>
+        
      
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 
 
 import { Box } from '@mui/material'
-import HomePage from './components/Home/HomePage'
 import Contact from './components/Contact'
 
 
